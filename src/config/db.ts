b237a05c956620564ee3d706b7841aef282b7aa9
noldee <188809273+noldee@users.noreply.github.com>
2026-09-09 23:50:01 -0500

    feat: migrate from jest to vitest

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,9 +1,12 @@
 import { Sequelize } from "sequelize-typescript";
 import dotenv from "dotenv";
+import Product from "../models/Product.module";
+
 dotenv.config();
 
 const db = new Sequelize(process.env.DATABASE_URL, {
-  models: [__dirname + "/../models/**/*.ts"],
+  models: [Product],
+  logging: false,
 });
 
 export default db;
