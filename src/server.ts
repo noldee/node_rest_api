296ce92293d154dfc635c272bac60622809a496c
noldee <188809273+noldee@users.noreply.github.com>
2026-09-12 20:52:53 -0500

    add cors

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,11 @@
+import dotenv from "dotenv";
+dotenv.config();
 import express from "express";
 import swaggerUI from "swagger-ui-express";
 import swaggerSpec from "./config/swagger";
 import router from "./router";
 import db from "./config/db";
+import cors, { CorsOptions } from "cors";
 
 // Conectar a la base de datos
 async function connectDB() {
@@ -20,6 +23,19 @@ connectDB();
 
 // Instancia de express
 const server = express();
+
+// Permitir conexiones
+const corsOptions: CorsOptions = {
+  origin: function (origin, callback) {
+    if (!origin || origin === process.env.FRONTEND_URL) {
+      callback(null, true);
+    } else {
+      callback(new Error("Error de CORS"));
+    }
+  },
+};
+
+server.use(cors(corsOptions));
 
 // Leer datos del formulario
 server.use(express.json());
